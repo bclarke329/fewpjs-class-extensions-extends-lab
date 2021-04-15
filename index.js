92b5cb288f124c9ff8e5709cab0fa9abab75bf32
bclarke329 <bmendoza329@hotmail.com>
2021-04-14 19:26:04 -0500

    commit tree be9f9d19f10e538fed19520448f319705374845a

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+class Polygon {
+    constructor(numOfSides) {
+        this.sides = sides
+        this.numOfSides = numOfSides;
+    }
+
+    get countSides() {
+        return this.numOfSides.length
+    }
+
+    get perimeter() {
+    
+    }
+
+}
+
+class Triangle extends Polygon{
+    
+}
+
+class Square extends Polygon {
+
+}
